5547aea66e31ca01a3ff0f8ade5d70bb03510d8e
Taufiq <tm8023009@gmail.com>
2026-04-16 10:54:15 +0530

    Adding my day 21 of web dev journey

---
+++ javascript/conditional_statements.js
@@ -0,0 +1,26 @@
+// if statements
+let age = 23;
+if (age >=18){
+    console.log("you can vote");
+
+}
+
+//else if statements 
+
+let a = 14;
+if(a >= 18){
+    console.log("You can vote");
+}
+else if(a <=18){
+    console.log("You cannot vote");
+}
+
+
+//else statement
+let x = 18;
+if(x >= 18){
+    console.log("You can vote");
+}
+else{
+    console.log("You cannot vote");
+}
