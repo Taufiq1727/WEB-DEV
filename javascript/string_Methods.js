af7a248dab16d7752457f54c1c41af35bbd944be
Taufiq <tm8023009@gmail.com>
2026-04-19 16:17:18 +0530

    Adding my day 24 of web dev journey

--- javascript/string_Methods.js
+++ javascript/string_Methods.js
@@ -1,6 +1,5 @@
 // Trim method
 
-let msg =  "      hello      ";
 // in the console type msg.trim(); to remove the extra spaces
 
 
@@ -16,6 +15,24 @@ let n = "Taufiq";
 
 console.log(n.toLowerCase());
 
-// methods of arguments 
 
+
+
+let msg =  "      hello      ";
+
+let newMsg = msg.trim().toUpperCase();
+console.log("Trimmed and uppercased message:", newMsg);
+
+// string slicing
 let str = "Ilovecoding";
+
+console.log(str.slice(0,4));
+console.log(str.slice(-1));
+
+//replacing
+
+console.log(str.replace("love","do"));
+
+//repeating
+
+console.log(str.repeat(4));
