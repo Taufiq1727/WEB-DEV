0dc14eb3536c6e1100b4f65d26cddff4a3e3bec9
Taufiq <tm8023009@gmail.com>
2026-04-26 19:31:18 +0530

    Adding my day 31 of my web dev jpurney

--- javascript/functions.js
+++ javascript/functions.js
@@ -105,3 +105,57 @@ const calculator = {
 console.log(calculator.add(1,3));
 console.log(calculator.sub(1,3));
 console.log(calculator.mul(1,3));
+
+
+//"this" statement in js 
+const student  ={
+    name:"Taufiq",
+    age:23,
+    eng:34,
+    math:87,
+    phy:89,
+    getAvg(){
+        let avg = (this.eng + this.math + this.phy)/3;
+        console.log(avg);
+        console.log(`${this.name} got avg marks = {avg}`);
+    }
+}
+
+// Error handling:
+console.log("Hello");
+console.log("Hello");
+try{
+    console.log(a);
+}catch{
+    console.log("This variable is not defined");
+}
+
+console.log("Hello");
+console.log("Hello");
+
+
+//Arrow function 
+
+const i = (a,b) =>{
+    return(a+b);
+}
+
+console.log(i(1,2));
+
+
+//set time out function
+
+console.log("Hi there");
+
+setTimeout(()=>{
+    console.log("Taufiq")
+},4000);
+console.log("Welcome"); 
+
+//set interval function
+// console.log("Hi there");
+
+// setInterval(()=>{
+//     console.log("Taufiq")
+// },2000);
+// console.log("Welcome"); 
