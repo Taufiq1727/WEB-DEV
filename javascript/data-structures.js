7a71fdec339f4c9989c8b70d7c1dc758044091ca
Taufiq <tm8023009@gmail.com>
2026-05-04 19:14:15 +0530

    Day 39: Completed promises and async JavaScript concepts
    - Learned callback hell and its issues
    - Implemented promises for async handling
    - Used then() and catch() methods
    - Practiced promise chaining
    - Handled results and errors in promises
    - Refactored old callback-based code

--- javascript/data-structures.js
+++ javascript/data-structures.js
@@ -1,13 +1,50 @@
-function one(){
-    return 1;
+function one() {
+  return 1;
 }
-function two(){
-    return one()+one();
+function two() {
+  return one() + one();
 }
-function three(){
-    let ans = two()+one();
-    console.log(ans)
-
-
+function three() {
+  let ans = two() + one();
+  console.log(ans);
 }
 three();
+
+//callback hell example
+h1 = document.querySelector("h1");
+function changeColor(color, delay, nextColorChange) {
+  setTimeout(() => {
+    h1.style.color = color;
+    if (nextColorChange) nextColorChange();
+  }, delay);
+}
+changeColor("red", 1000, () => {
+  changeColor("orange", 1000, () => {
+    changeColor("green", 1000, () => {
+      changeColor("yellow", 1000, () => {
+        changeColor("grey", 1000);
+      });
+    });
+  });
+});
+
+//setting up promise
+
+function savetoDb(data, success, failure) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 3) {
+    success();
+  } else {
+    failure();
+  }
+}
+
+savetoDb(
+  "Taufiq",
+  () => {
+    console.log("Success: Data was saved");
+  },
+  () => {
+    console.log("failure :Weak internet connection");
+  },
+);
