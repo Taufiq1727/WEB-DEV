0f983d67c6d28b5f06df3177e0a3894304cd892f
Taufiq <tm8023009@gmail.com>
2026-05-16 21:36:04 +0530

    Day 46-48: Practiced and revised JavaScript concepts
    - Revised core JavaScript fundamentals
    - Practiced functions, arrays, objects, and loops
    - Strengthened DOM and async JS concepts
    - Solved practice problems for better understanding
    - Focused on consistency alongside exam preparation

--- practice_Questions/app.js
+++ practice_Questions/app.js
@@ -32,4 +32,45 @@ let p = document.createElement("p")
 p.innerHTML = "<b>Hello World</b>";
 document.querySelector("body").append(p);
 
+//  Create a button on the page using JavaScript. Add an event listener to the button
+// that changes the button’s color to green when it is clicked.
+
+
+//Ans:
+let btns = document.createElement("button");
+btns.innerText = "click";
+btns.addEventListener("click", function(){
+    btns.style.backgroundColor = "green";
+
+});
+document.querySelector("body").append(btns);
+
+
+//  Create an input element on the page with a placeholder ”enter your name” and an
+// H2 heading on the page inside HTML.
+// The purpose of this input element is to enter a user’s name so it should only input
+// letters from a-z, A-Z and space (all other characters should not be detected).
+// Whenever the user inputs their name, their input should be dynamically visible inside
+// the heading.
+// [Please note that no other character apart from the allowed characters should be
+// visible in the heading]
+
+
+
+let inp = document.createElement("input");
+inp.setAttribute("placeholder","enter your name");
+let h2 = document.createElement("h2");
+document.querySelector("body").append(inp);
+document.querySelector("body").append(h2);
+
+
+inp.addEventListener("input", function () {
+
+    // Remove all characters except letters and spaces
+    let filteredValue = inp.value.replace(/[^a-zA-Z ]/g, "");
+
+    // Show filtered value inside heading
+    h2.innerText = filteredValue;
+});
+
 
