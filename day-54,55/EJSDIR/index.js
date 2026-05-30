ea9a6321e9163c75b4a8d13a50cc68330028d892
Taufiq <tm8023009@gmail.com>
2026-05-30 18:25:41 +0530

    Day 55: Completed EJS templating concepts and mini project
    - Completed EJS fundamentals
    - Practiced conditional rendering and loops
    - Learned reusable templates with includes
    - Built dynamic pages using Express and EJS
    - Worked on Instagram-style EJS project
    - Strengthened backend rendering concepts

--- day-54,55/EJSDIR/index.js
+++ day-54,55/EJSDIR/index.js
@@ -17,6 +17,14 @@ app.get("/rolldice", (req, res) => {
   res.render("rolldice.ejs");
 });
 
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  console.log(data);
+  res.render("instagram.ejs", { data });
+});
+
 app.listen(port, () => {
   console.log(`listining on port ${port}`);
 });
