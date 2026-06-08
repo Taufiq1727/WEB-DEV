a1397308b9b7f90836ce942786824208159c88df
Taufiq <tm8023009@gmail.com>
2026-06-08 21:48:35 +0530

    Day 63: Started Node.js with SQL integration
    - Learned Faker package basics
    - Explored MySQL package for Node.js
    - Practiced SQL commands through CLI
    - Created user table
    - Inserted records into database
    - Began connecting Node.js with MySQL

---
+++ day-63/SQLCLASS/index.js
@@ -0,0 +1,35 @@
+const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "app",
+  password: "Taushi1727@",
+});
+
+const getRandomUser = () => {
+  return [
+     faker.string.uuid(),
+     faker.internet.username(),
+     faker.internet.email(),
+     faker.internet.password(),
+  ];
+};
+let q = "INSERT INTO user(id,username,email,password) VALUES ?"
+let data = [];
+for(let i = 0 ; i<=100;i++){
+    data.push(getRandomUser());
+}
+
+
+try {
+  connection.query(q,[data], (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+connection.end();
