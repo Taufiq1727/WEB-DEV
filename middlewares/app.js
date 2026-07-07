bcea05bb603d878977f274ce839039fc12278fb3
Taufiq <tm8023009@gmail.com>
2026-07-07 19:04:18 +0530

    Day 78: Completed Express middleware and error handling
    - Learned utility middlewares and app.use()
    - Implemented multiple middleware functions
    - Explored API token authentication using query strings
    - Practiced Express error handling
    - Strengthened understanding of Express request lifecycle

--- middlewares/app.js
+++ middlewares/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
+//app.use((req, res, next) => {
+//   console.log("Middleware executed");
+//   next();
+// });
+
+app.use("/random", (req, res, next) => {
   console.log("Middleware executed");
   next();
 });
 
+app.get;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.get("/random", (req, res) => {
   res.send("Random route");
+});
+
+// -----logger middleware-----
+app.use((req, res, next) => {
+  req.time = new Date(Date.now()).toDateString();
+  console.log(req.method, req.path, req.time);
+  next();
+});
+// 404 page
+app.use((req, res, next) => {
+  res.status(404).send("Route not found");
 });
 
 app.listen(3000, () => {
