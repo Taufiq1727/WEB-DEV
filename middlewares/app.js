b81b063562a23e24363c94b4323c5dc59287b756
Taufiq <tm8023009@gmail.com>
2026-07-08 19:16:31 +0530

    Day 79: Started Express error handling module
    - Learned Express error handling middleware
    - Implemented custom error class
    - Explored default status codes and error messages
    - Improved backend error handling fundamentals

--- middlewares/app.js
+++ middlewares/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 //app.use((req, res, next) => {
 //   console.log("Middleware executed");
 //   next();
 // });
 
-app.use("/random", (req, res, next) => {
-  console.log("Middleware executed");
-  next();
+// app.use("/random", (req, res, next) => {
+//   console.log("Middleware executed");
+//   next();
+// });
+const checkToken = (req, res, next) => {
+  const token = req.query.token;
+  if (token === "12345") {
+    return next();  
+  }
+  throw new ExpressError("You need a token", 401);
+};
+app.get("/api", checkToken, (req, res) => {
+  res.send("You have access to the API");
 });
-
-app.get;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -20,16 +29,23 @@ app.get("/random", (req, res) => {
 });
 
 // -----logger middleware-----
-app.use((req, res, next) => {
-  req.time = new Date(Date.now()).toDateString();
-  console.log(req.method, req.path, req.time);
-  next();
-});
+// app.use((req, res, next) => {
+//   req.time = new Date(Date.now()).toDateString();
+//   console.log(req.method, req.path, req.time);
+//   next();
+// });
 // 404 page
-app.use((req, res, next) => {
-  res.status(404).send("Route not found");
-});
+// app.use((req, res, next) => {
+//   res.status(404).send("Route not found");
+// });
 
+app.get("/error", (req, res) => {
+  abcd=abcdcd;
+});
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
